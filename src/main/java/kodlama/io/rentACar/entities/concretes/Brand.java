c04f5a5eedbf4b656929f70191ed8ac1290c4648
RafetJinx <rafet.ersoy.cs@gmail.com>
2023-03-05 23:47:25 +0300

    "(2022) Java Yazılım Geliştirici Yetiştirme Kampı 4: Spring Boot Giriş" dersinin kodları

---
+++ src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -0,0 +1,15 @@
+package kodlama.io.rentACar.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Brand {
+    private int id;
+    private String name;
+}
